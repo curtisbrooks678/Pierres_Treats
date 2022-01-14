9bfdfcd6f6ecab763f9f8d727de27629dcc8f0f4
Curtis Brooks <curtisbrooks678@gmail.com>
2022-01-14 14:06:57 -0800

    add authorization for home page

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    [Authorize]
     [HttpGet("/")]
     public ActionResult Index()
     {
