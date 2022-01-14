82487636e0c9540b4becbe7c2432ecebc6038343
Curtis Brooks <curtisbrooks678@gmail.com>
2022-01-14 14:31:18 -0800

    add user list of flavors and treats on home page

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,14 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierresTreats.Controllers
 {
+  [Authorize]
   public class HomeController : Controller
   {
-    [Authorize]
-    [HttpGet("/")]
-    public ActionResult Index()
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public HomeController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
     {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      ViewData["userTreats"] = _db.Treats
+        .Where(entry => entry.User.Id == currentUser.Id)
+        .ToList();
+      ViewData["userFlavors"] = _db.Flavors
+        .Where(entry => entry.User.Id == currentUser.Id)
+        .ToList();
       return View();
     }
   }
